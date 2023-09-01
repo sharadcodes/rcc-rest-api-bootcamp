0ae0ea9645dc33b696e3269333d322340c078c8b
Sharad Raj <dev_sharad@outlook.com>
2023-09-01 23:42:31 +0530

    Made modular

--- app.js
+++ app.js
@@ -1,74 +1,28 @@
 const express = require("express");
 const crypto = require("crypto");
+const morgan = require("morgan");
 // const { randomUUID } = require("crypto");
-
 const app = express();
-
-app.use(express.json());
-
 const PORT = 3000;
 
-const students = [
-  { id: crypto.randomUUID(), name: "John" }, // 0 index
-  { id: crypto.randomUUID(), name: "Tom" }, // 1 index
-];
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
-app.get("/", function (req, res) {
-  return res.send("Hello World");
-});
+// routes
+const studentRoutes = require("./routes/student");
 
-app.get("/student_data", function (req, res) {
-  return res.status(200).json(students);
-});
+// root route
 
-app.get("/student_data/:id", function (req, res) {
-  const id = req.params.id;
-  // const className = req.params.className;
-  // const roll_no = req.params.roll_no;
-  // const { id, className, roll_no } = req.params;
-  let student = null;
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == id) {
-      student = students[i];
-      break;
-    }
-  }
-  if (student == null) {
-    return res.status(404).json({
-      error: "Student not found",
-    });
-  } else {
-    return res.status(200).json(student);
-  }
+app.get("/", (req, res) => {
+  return res.send("Pranam! 🙏🏻");
 });
 
-app.post("/student_data", function (req, res) {
-  const name = req?.body?.name;
-  if (!name) {
-    return res.status(400).json({ error: "Name is required" });
-  }
-  students.push({
-    id: crypto.randomUUID(),
-    name: name,
-  });
-  return res.status(201).json({
-    message: "Student added successfully",
-    students: students,
-  });
-});
-app.put("/student_data/:id", function (req, res) {
-  console.log(req.params, req.body);
-  for (let i = 0; i < students.length; i++) {
-    if (students[i].id == req.params.id) {
-      students[i].name = req.body.name;
-      break;
-    }
-  }
-  return res.status(200).json({ message: "Success", students });
-});
+// use the imported routes
+app.use("/students", studentRoutes);
 
 // this line starts the server
 app.listen(PORT, function () {
-  console.log("Server is running on port " + PORT);
+  console.log("Server is running on http://localhost:" + PORT);
   // http://localhost:3000/ open this in browser to see the output
 });
